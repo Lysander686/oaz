fcad3f325d18d6c3c22ca73c10f52fcb91e662d8
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-04-12 20:38:29 +0800

    Added static resources.  Configured encoding filter.

---
+++ oaz_web/src/main/java/com/example/oaz/global/EncodingFilter.java
@@ -0,0 +1,25 @@
+package com.example.oaz.global;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+  private String encoding = "UTF-8";
+
+  public void init(FilterConfig filterConfig) throws ServletException {
+    if (filterConfig.getInitParameter("encoding") != null) {
+      encoding = filterConfig.getInitParameter("encoding");
+    }
+  }
+
+
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
+    request.setCharacterEncoding(encoding);
+    response.setCharacterEncoding(encoding);
+    chain.doFilter(request, response);
+  }
+}
